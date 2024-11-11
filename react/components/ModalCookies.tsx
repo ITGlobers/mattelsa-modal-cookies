abf7e48b3276aaad490cd79136b6abf69e91c060
Lennin Ibarra <ing.lenninibarra@gmail.com>
2024-11-11 17:36:36 -0500

    feature/modal-cookies: se implementan botones para cerrar el modal

--- react/components/ModalCookies.tsx
+++ react/components/ModalCookies.tsx
@@ -1,21 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { useCssHandles } from 'vtex.css-handles';
+import './styles.css';
 
 type Props = {
   titleCookies: string
   messageCookies: string
 }
 
-const ModalCookies = ({titleCookies, messageCookies}: Props) =>{
-  console.log(titleCookies, messageCookies);
+const CSS_HANDLES = ['container', 'buttons', 'buttonAccept', 'buttonCancel']
+
+const ModalCookies = ({ titleCookies, messageCookies }: Props) => {
+  const handles = useCssHandles(CSS_HANDLES)
+  const [isVisible, setIsVisible] = useState(true) // Estado para controlar la visibilidad del modal
+
+  const closeModal = () => {
+    setIsVisible(false)
+  }
+
+  if (!isVisible) return null // Si el modal no es visible, no renderizar nada
 
   return (
     <>
-      <p>{titleCookies}</p>
-      <p>{messageCookies}</p>
+      <div className={`${handles.container}`}>
+        <p className="white f8 b">{titleCookies}</p>
+        <p className="white f6 b">{messageCookies}</p>
+        <div className={handles.buttons}>
+          <button className={handles.buttonAccept} onClick={closeModal}>Aceptar</button>
+          <button className={handles.buttonCancel} onClick={closeModal}>Cancelar</button>
+        </div>
+      </div>
     </>
   )
-
 }
 
 ModalCookies.propTypes = {
@@ -24,8 +40,28 @@ ModalCookies.propTypes = {
 }
 
 ModalCookies.defaultProps = {
-  titleCookies: 'Titulo del componente Cookies',
-  messageCookies: 'Aquí va el mensaje de aprobación de cookies para el usuario'
+  titleCookies: "Titulo del componente Cookies",
+  messageCookies: "Aquí va el mensaje de aprobación de cookies para el usuario"
+}
+
+ModalCookies.schema = {
+  title: "modal informativo para cookies",
+  type: "object",
+  properties: {
+    titleCookies: {
+      title:"Titulo para el modal",
+      description: "Agrega un titulo para el modal",
+      type: "string"
+    },
+    messageCookies:{
+      title: "Mensaje para el modal",
+      description: "Agrega un mensaje para el modal",
+      type: "string",
+      widget: {
+        "ui:widget":"textarea"
+      }
+    }
+  }
 }
 
 export default ModalCookies;
