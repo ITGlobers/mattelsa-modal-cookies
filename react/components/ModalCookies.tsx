8f32594c70bd730cc7381b3da616cafb205f0517
Lennin Ibarra <ing.lenninibarra@gmail.com>
2024-11-11 18:26:20 -0500

    feature/modal-cookies: se aplican estilos al modal

--- react/components/ModalCookies.tsx
+++ react/components/ModalCookies.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { useCssHandles } from 'vtex.css-handles';
 import './styles.css';
@@ -8,23 +8,42 @@ type Props = {
   messageCookies: string
 }
 
-const CSS_HANDLES = ['container', 'buttons', 'buttonAccept', 'buttonCancel']
+const CSS_HANDLES = [
+  'container',
+  'buttons',
+  'buttonAccept',
+  'buttonCancel',
+  'container__tittle',
+  'container__paragraph']
 
 const ModalCookies = ({ titleCookies, messageCookies }: Props) => {
   const handles = useCssHandles(CSS_HANDLES)
-  const [isVisible, setIsVisible] = useState(true) // Estado para controlar la visibilidad del modal
+
+  // Verificar si el usuario ya aceptó los términos
+  const [isVisible, setIsVisible] = useState(() => {
+    return localStorage.getItem("cookiesAccepted") !== "true";
+  });
 
   const closeModal = () => {
-    setIsVisible(false)
+    setIsVisible(false);
+    localStorage.setItem("cookiesAccepted", "true"); // Guardar en localStorage
   }
 
-  if (!isVisible) return null // Si el modal no es visible, no renderizar nada
+  useEffect(() => {
+    // Comprobar al cargar el componente si el usuario ya aceptó los términos
+    const cookiesAccepted = localStorage.getItem("cookiesAccepted");
+    if (cookiesAccepted === "true") {
+      setIsVisible(false);
+    }
+  }, []);
+
+  if (!isVisible) return null; // Si el modal no es visible, no renderizar nada
 
   return (
     <>
-      <div className={`${handles.container}`}>
-        <p className="white f8 b">{titleCookies}</p>
-        <p className="white f6 b">{messageCookies}</p>
+      <div className={handles.container}>
+        <p className={handles.container__tittle}>{titleCookies}</p>
+        <p className={handles.container__paragraph}>{messageCookies}</p>
         <div className={handles.buttons}>
           <button className={handles.buttonAccept} onClick={closeModal}>Aceptar</button>
           <button className={handles.buttonCancel} onClick={closeModal}>Cancelar</button>
